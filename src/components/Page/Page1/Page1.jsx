783ffcd47be6ddda965f95c8bed86acaceea7098
sasayan0315 <sasayan.sp@gmail.com>
2022-02-21 12:42:29 +0900

    location state

--- src/components/Page/Page1/Page1.jsx
+++ src/components/Page/Page1/Page1.jsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
 export const Page1 = () => {
+  const arr = [...Array(100).keys()];
   return (
     <>
       <h1> Page1 </h1>
-      <Link to="/page1/detail1/">Detail 1</Link>
+      <Link to={{ pathname: "/page1/detail1/", state: arr }}>Detail 1</Link>
       <Link to="/page1/detail2/">Detail 2</Link>
     </>
   );
--- src/components/Page/Page1/Page1Detail1.jsx
+++ src/components/Page/Page1/Page1Detail1.jsx
@@ -1,7 +1,15 @@
+import { useLocation } from "react-router-dom";
+
 export const Page1Detail1 = () => {
+  const location = useLocation();
+  console.log(location.state);
+
   return (
     <>
       <h1> Page1 Detail1 </h1>
+      {location.state.map((val) => {
+        return <p>{val}</p>;
+      })}
     </>
   );
 };
